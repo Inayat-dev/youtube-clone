148f63407b57595eb1ed97a1f6482048ad6a3a79
inayat-dev <bukerainayat9737@gmail.com>
2026-07-19 16:22:30 +0530

    database connection

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { database } from "../constants.js";
+
+const connectDB = async ()=>{
+    try{
+        const coni = mongoose.connect(`${process.env.MONGODB_URI}/${database}`) 
+        
+        console.log("database connected successfuly : "+coni)
+        
+    }catch(err){
+        console.log("Error in Connecting DataBase :"+err)
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"
+
+dotenv.config({ path: "./.env" });
+
+const app = express()
+connectDB()
+
+app.get("/",(req,res)=>{
+    res.json({data:"api is running"})
+})
+
+app.listen(4500,()=>{
+    console.log("server running or 4500 port")
+})
