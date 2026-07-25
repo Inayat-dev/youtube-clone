0595d273bde77c40c0db3d5925cdfdfae00174fd
inayat-dev <bukerainayat9737@gmail.com>
2026-07-25 07:34:59 +0530

    multer setup

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+import path from "path"
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"./public/temp")
+    },
+    filename:function(req,file,cb){
+        const fileName = Date.now() + path.extname(file.originalname)
+        cb(null,filename)
+    }
+})
+
+export const upload = multer({storage})
