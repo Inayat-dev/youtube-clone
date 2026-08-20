77516e4035804ff72f5a51cef9f3adbd50bc1b59
inayat-dev <bukerainayat9737@gmail.com>
2026-08-20 20:44:05 +0530

    debug

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -9,7 +9,7 @@ const playlistSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    video:[{
+    videoS:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:"Video"
     }],
