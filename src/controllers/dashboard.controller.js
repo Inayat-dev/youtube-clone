542f4147c278b72aa900a9342e27411b7fd5abc4
inayat-dev <bukerainayat9737@gmail.com>
2026-08-23 14:59:20 +0530

    dashboard controllers are done

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose"
+import {Video} from "../models/video.model.js"
+import {Subscription} from "../models/subscription.model.js"
+import {Like} from "../models/like.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getChannelStats = asyncHandler(async (req, res) => {
+    const channelId = req.user._id;
+
+    const videoStats = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "likes"
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalViews: { $sum: "$views" },
+                totalVideos: { $sum: 1 },
+                totalLikes: { $sum: { $size: "$likes" } }
+            }
+        }
+    ]);
+
+    const totalSubscribers = await Subscription.countDocuments({
+        channel: channelId
+    });
+
+    const stats = {
+        totalViews: videoStats[0]?.totalViews || 0,
+        totalVideos: videoStats[0]?.totalVideos || 0,
+        totalLikes: videoStats[0]?.totalLikes || 0,
+        totalSubscribers
+    };
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, stats, "Channel stats fetched successfully"));
+});
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+    // TODO: Get all the videos uploaded by the channel
+    const videos = await Video.aggregate([
+        {
+            $match:{owner:new mongoose.Types.ObjectId(req.user._id)}
+        }
+    ])
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, videos, "Channel videos fetched successfully"));
+})
+
+export {
+    getChannelStats, 
+    getChannelVideos
+}
