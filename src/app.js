daa88ea06517c6aaebfbe7652683f0ab81b032aa
inayat-dev <bukerainayat9737@gmail.com>
2026-09-17 20:43:26 +0530

    add new url

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import ApiError from "./utils/ApiError.js";
 const app = express()
 
 app.use(cors({
-    origin:['http://localhost:5173', 'https://youtube-clone---frontend.pages.dev'],
+    origin:['http://localhost:5173', 'https://youtube-clone---frontend.pages.dev',"http://my-youtube-clone.pages.dev"],
     credentials: true
 }))
 app.use(express.json({limit:"16kb"}))
